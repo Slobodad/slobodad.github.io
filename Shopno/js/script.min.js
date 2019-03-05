9aef8d18d06aaf8a670f1e47dbcacb08e03ea0b2
Slobodad <44872516+Slobodad@users.noreply.github.com>
2019-03-05 19:46:13 +0200

    commit

--- Shopno/js/script.min.js
+++ Shopno/js/script.min.js
@@ -2,218 +2,142 @@
 
 $(document).ready(function(){
 
-    // Scroll
-    $("a").on("click", function(event) {
-        if (this.hash !== "") {
-        event.preventDefault();
+  // Scroll
+  $("a").on("click", function(event) {
+      if (this.hash !== "") {
+      event.preventDefault();
 
-        var hash = this.hash;
+      var hash = this.hash;
 
-        $("html, body").animate(
-            {
-            scrollTop: $(hash).offset().top
-            },
-            1000,
-            function() {
-            window.location.hash = hash;
-            }
-        );
-        }
-    });
-
-    // Header menu
-
-    var menuBtn = $('.js-header-menu');
-    var menu = $('.js-menu');
-
-    menuBtn.on('click', function(){
-        menu.toggleClass('modal__menu--is-active')
-    });
-
-    $('.menu__item').on('click', function() {
-      menu.removeClass('modal__menu--is-active');
-    });
-    // Home modal btn
-
-    var homeMadalBtn = $('.js-modal-btn');
-    var modalBox = $('.js-modal-box');
-    var sendBtn = $('.js-btn-send');
-    var closeBtn = $('.js-btn-close');
-
-    homeMadalBtn.on('click', function(){
-        modalBox.toggleClass('modal__btn-box--is-active');
-    });
-
-    sendBtn.on('click', function(){
-        modalBox.removeClass('modal__btn-box--is-active');
-    });
-
-    closeBtn.on('click', function(){
-        modalBox.removeClass('modal__btn-box--is-active');
-    });
-
-    // Slider section about
-
-    $('.about__slider').slick({
-        centerMode:false,     
-        draggable:false,    
-        arrows:false,    
-        autoplay:true,
-        autoplaySpeed:3000,     
-        slidesToShow:1,     
-        slidesToScroll:1,   
-        dots:true,
-
-    });
-
-     // Slider section team-say
-
-     $('.team-say__slider').slick({
-        centerMode:false,     
-        draggable:false,    
-        arrows:true,    
-        autoplay:false,
-        autoplaySpeed:3000,     
-        slidesToShow:1,     
-        slidesToScroll:1, 
-        nextArrow: $('.right-arrow'),
-        prevArrow: $('.left-arrow'),
-        dots:true,
-
-    });
-
-    // Rating 1
-
-    var rating1 = function(nowPos1) {
-        // У всех убираем active
-        $('.rating-1 .rating-1__star').removeClass('rating-1__star--active');
-        
-        for (var i = 0; nowPos1 + 1 > i; i++) {
-          $('.rating-1 .rating-1__star').eq(i).toggleClass('rating-1__star--active');
-        }
+      $("html, body").animate(
+          {
+          scrollTop: $(hash).offset().top
+          },
+          1000,
+          function() {
+          window.location.hash = hash;
+          }
+      );
       }
-      // переменная содержит количество активных звезд
-      var starsCount1 = $('.rating-1__star.rating-1__star--active').length; 
-      
-      // При наведении
-      $('.rating-1 .rating-1__star').hover(function() {
-        rating1($(this).index());
-      });
-      
-      // При клике
-      $('.rating-1 .rating-1__star').click(function() {
-        rating1($(this).index());
-        // меняем количество по клику
-        starsCount1 = $('rating-1__star.rating-1__star--active').length; 
-      });
-      
-      // Как только отводим мышку, возвращаем количество активных айтемов, которые были изначально
-      $('.rating-1 .rating-1__star').on('mouseleave', function() {
-        rating1(+starsCount1 - 1);
-      });
+  });
 
-    // Rating2
+  // Header menu
+
+  var menuBtn = $('.js-header-menu');
+  var menu = $('.js-menu');
+
+  menuBtn.on('click', function(){
+      menu.toggleClass('modal__menu--is-active')
+  });
+
+  $('.menu__item').on('click', function() {
+    menu.removeClass('modal__menu--is-active');
+  });
+  // Home modal btn
+
+  var homeMadalBtn = $('.js-modal-btn');
+  var modalBox = $('.js-modal-box');
+  var sendBtn = $('.js-btn-send');
+  var closeBtn = $('.js-btn-close');
+
+  homeMadalBtn.on('click', function(){
+      modalBox.toggleClass('modal__btn-box--is-active');
+  });
+
+  sendBtn.on('click', function(){
+      modalBox.removeClass('modal__btn-box--is-active');
+  });
+
+  closeBtn.on('click', function(){
+      modalBox.removeClass('modal__btn-box--is-active');
+  });
+
+  // Slider section about
+
+  $('.about__slider').slick({
+      centerMode:false,     
+      draggable:false,    
+      arrows:false,    
+      autoplay:true,
+      autoplaySpeed:3000,     
+      slidesToShow:1,     
+      slidesToScroll:1,   
+      dots:true,
+
+  });
+
+    // Slider section team-say
+
+    $('.team-say__slider').slick({
+      centerMode:false,     
+      draggable:false,    
+      arrows:true,    
+      autoplay:false,
+      autoplaySpeed:3000,     
+      slidesToShow:1,     
+      slidesToScroll:1, 
+      nextArrow: $('.right-arrow'),
+      prevArrow: $('.left-arrow'),
+      dots:true,
+
+  });
+
+  // Raiting
+
+  // Click 
+  var addClass = function(id, index) {
+    $(id + ' .raiting__star').removeClass('raiting__star--active');
+
+    for (var i = 0; i < index; i++) {
+      $(id + ' .raiting__btn').eq(i).children().addClass('raiting__star--active');
+    }
+  };
+
+  $('.raiting').click(function(e) {
+    var itemId = '#' + $(this).attr('id');
+    var rateIndex = e.target.closest('button').dataset.rate;
+
+    $(itemId + ' .raiting__btn').removeClass('js-active');
+ 
+    for (var i = 0; i < rateIndex; i++) {
+      $(itemId + ' .raiting__btn').eq(i).addClass('js-active');
+    }
+
+    addClass(itemId, rateIndex);
+  });
+
+  // Hover
+  $('.raiting__btn').mouseover(function(e) {
+    var slideId = '#' + $(this).parent('div').attr('id'); 
+    var rateIndex = $(this).data('rate');
+
+    addClass(slideId, rateIndex);
+  });
+
+  $('.raiting__btn').mouseleave(function(e) {
+    var slideId = '#' + $(this).parent('div').attr('id'); 
+    var items = $(slideId + ' .raiting__btn');
+    var activeItems = 0;
     
-
-    var rating2 = function(nowPos2) {
-      // У всех убираем active
-      $('.rating-2 .rating-2__star').removeClass('rating-2__star--active');
-      
-      for (var i = 0; nowPos2 + 1 > i; i++) {
-        $('.rating-2 .rating-2__star').eq(i).toggleClass('rating-2__star--active');
+    for (var i = 0; i < items.length; i++) {
+      if (items.eq(i).hasClass('js-active')) {
+        activeItems += 1;
       }
     }
-    // переменная содержит количество активных звезд
-    var starsCount2 = $('.rating-2__star.rating-2__star--active').length; 
-    
-    // При наведении
-    $('.rating-2 .rating-2__star').hover(function() {
-      rating2($(this).index());
-    });
-    
-    // При клике
-    $('.rating-2 .rating-2__star').click(function() {
-      rating2($(this).index());
-      // меняем количество по клику
-      starsCount2 = $('rating-2__star.rating-2__star--active').length; 
-    });
-    
-    // Как только отводим мышку, возвращаем количество активных айтемов, которые были изначально
-    $('.rating-2 .rating-2__star').on('mouseleave', function() {
-      rating2(+starsCount2 - 1);
-    });
 
-    // Rating 3
+    addClass(slideId, activeItems);
+  });
 
+  // Popup section services
 
-    var rating3 = function(nowPos3) {
-      // У всех убираем active
-      $('.rating-3 .rating-3__star').removeClass('rating-3__star--active');
-      
-      for (var i = 0; nowPos3 + 1 > i; i++) {
-        $('.rating-3 .rating-3__star').eq(i).toggleClass('rating-3__star--active');
-      }
-    }
-    // переменная содержит количество активных звезд
-    var starsCount3 = $('.rating-3__star.rating-3__star--active').length; 
-    
-    // При наведении
-    $('.rating-3 .rating-3__star').hover(function() {
-      rating3($(this).index());
-    });
-    
-    // При клике
-    $('.rating-3 .rating-3__star').click(function() {
-      rating3($(this).index());
-      // меняем количество по клику
-      starsCount3 = $('rating-3__star.rating-3__star--active').length; 
-    });
-    
-    // Как только отводим мышку, возвращаем количество активных айтемов, которые были изначально
-    $('.rating-3 .rating-3__star').on('mouseleave', function() {
-      rating3(+starsCount3 - 1);
-    });
+  $('.blog__btn').click(function(e) {
+    var elemId = '#js-popup-' + $(this).data('order');
+    $(elemId).addClass('blog__popup--is-active');
+  });
 
-    // Rating 4
-
-    var rating4 = function(nowPos4) {
-      // У всех убираем active
-      $('.rating-4 .rating-4__star').removeClass('rating-4__star--active');
-      
-      for (var i = 0; nowPos4 + 1 > i; i++) {
-        $('.rating-4 .rating-4__star').eq(i).toggleClass('rating-4__star--active');
-      }
-    }
-    // переменная содержит количество активных звезд
-    var starsCount4 = $('.rating-4__star.rating-4__star--active').length; 
-    
-    // При наведении
-    $('.rating-4 .rating-4__star').hover(function() {
-      rating4($(this).index());
-    });
-    
-    // При клике
-    $('.rating-4 .rating-4__star').click(function() {
-      rating4($(this).index());
-      // меняем количество по клику
-      starsCount4 = $('rating-4__star.rating-4__star--active').length; 
-    });
-    
-    // Как только отводим мышку, возвращаем количество активных айтемов, которые были изначально
-    $('.rating-4 .rating-4__star').on('mouseleave', function() {
-      rating4(+starsCount4 - 1);
-    });
-
-    // Popup section services
-
-    $('.blog__btn').click(function(e) {
-      var elemId = '#js-popup-' + $(this).data('order');
-      $(elemId).addClass('blog__popup--is-active');
-    });
-
-    $('.js-blog-close').on('click', function(){
-        $(this).closest('div').removeClass('blog__popup--is-active');
-    });  
+  $('.js-blog-close').on('click', function(){
+    $(this).closest('div').removeClass('blog__popup--is-active');
+  });  
 });
 
 // Google map
